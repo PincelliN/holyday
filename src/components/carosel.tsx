c9f06ad7bbebb050583039cc8870c62a8b227c3f
Nicolò Pincelli <pincelli.n@gmail.com>
2025-06-06 12:39:06 +0200

    fix: carosel

--- src/components/carosel.tsx
+++ src/components/carosel.tsx
@@ -1,69 +1,83 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllHoliday } from "../api/holiday";
+import { Travel } from "../types/holiday";
+import CaroselItem from "./carosel-item";
+import ButtonSlide from "./button-slide";
+import Loader from "./loader";
+
 export default function Carosel() {
+  const [holidays, setHolidays] = useState<Travel[]>([]);
+  const [isActive, setIsActive] = useState(0);
+
   const fetchHoliday = async () => {
     try {
       const response = await getAllHoliday();
-      console.log(response);
+      const result = response.data;
+      setHolidays(result);
     } catch (error) {
       console.log("Recupero vacanze fallito", error);
     }
   };
 
+  const handleNext = () => {
+    if (!holidays.length) return;
+    setIsActive((prev) => (prev === holidays.length - 1 ? 0 : prev + 1));
+  };
+
+  const handlePrev = () => {
+    if (!holidays.length) return;
+    setIsActive((prev) => (prev === 0 ? holidays.length - 1 : prev - 1));
+  };
+
   useEffect(() => {
     fetchHoliday();
   }, []);
+
+  if (!holidays.length) {
+    return <Loader />;
+  }
+
   return (
-    <>
-      <div id="carouselExampleCaptions" className="carousel slide">
-        <div className="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
-        </div>
-        <div className="carousel-inner"></div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+    <div id="carouselExampleCaptions" className="carousel slide w-75 h-75">
+      <div className="carousel-indicators">
+        {holidays.map((holiday, index) => (
+          <ButtonSlide
+            key={`${holiday.id}-buttonSlide`}
+            index={index}
+            activeIndex={isActive}
+          />
+        ))}
       </div>
-    </>
+      <div className="carousel-inner">
+        {holidays.map((holiday, index) => (
+          <CaroselItem
+            key={holiday.id}
+            title={holiday.titolo}
+            description={holiday.descrizione}
+            duration={holiday.durata}
+            money={holiday.prezzo}
+            image={holiday.img}
+            index={index}
+            activeIndex={isActive}
+          />
+        ))}
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        onClick={handlePrev}
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true" />
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        onClick={handleNext}
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true" />
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
   );
 }
