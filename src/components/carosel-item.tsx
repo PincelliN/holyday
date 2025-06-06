90605ae8ac6f75ef7776b16eaa12d4f898b0d0c6
Nicolò Pincelli <pincelli.n@gmail.com>
2025-06-06 11:04:56 +0200

    feat: carosel-item

---
+++ src/components/carosel-item.tsx
@@ -0,0 +1,13 @@
+export default function CaroselItem() {
+  return (
+    <>
+      <div className="carousel-item active">
+        <img src="..." className="d-block w-100" alt="..." />
+        <div className="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+    </>
+  );
+}
