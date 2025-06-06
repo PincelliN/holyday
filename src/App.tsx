a94d77990414c23c64d15f9d8ff4f1e1120e73d6
Nicolò Pincelli <pincelli.n@gmail.com>
2025-06-06 11:30:47 +0200

    feat: add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "./App.css";
+import Carosel from "./components/carosel";
 
 function App() {
   return (
     <div className="App">
       <h1>Le Nostre vacanze</h1>
+      <Carosel />
     </div>
   );
 }
